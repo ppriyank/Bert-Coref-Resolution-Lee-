d902e74dce2cdf515236175b816001f2ec0c6527
Yada Pruksachatkun <yp913@nyu.edu>
2019-04-21 18:14:13 -0400

    added example keys

--- coref_model_orig.py
+++ coref_model_orig.py
@@ -20,54 +20,55 @@ import conll
 import metrics
 
 import bert 
+from bert import modeling
+
+def model_fn_builder(bert_config, init_checkpoint, layer_indexes, use_tpu,
+                     use_one_hot_embeddings):
+    def model_fn(features, mode):  # pylint: disable=unused-argument
+        unique_ids = features["unique_ids"]
+        input_ids = features["input_ids"]
+        input_mask = features["input_mask"]
+        input_type_ids = features["input_type_ids"]
+        model = modeling.BertModel(
+            config=bert_config,
+            is_training=True,
+            input_ids=input_ids,
+            input_mask=input_mask,
+            token_type_ids=input_type_ids,
+            use_one_hot_embeddings=use_one_hot_embeddings)
+        if mode != tf.estimator.ModeKeys.PREDICT:
+            raise ValueError("Only PREDICT modes are supported: %s" % (mode))
+        tvars = tf.trainable_variables()
+        scaffold_fn = None
+        (assignment_map,
+         initialized_variable_names) = modeling.get_assignment_map_from_checkpoint(
+             tvars, init_checkpoint)
+        if use_tpu:
+            def tpu_scaffold():
+                tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+                return tf.train.Scaffold()
+            scaffold_fn = tpu_scaffold
+        else:
+            tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+        tf.logging.info("**** Trainable Variables ****")
+        for var in tvars:
+            init_string = ""
+            if var.name in initialized_variable_names:
+                init_string = ", *INIT_FROM_CKPT*"
+            tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape,
+                          init_string)
+        all_layers = model.get_all_encoder_layers()
+        predictions = {
+            "unique_id": unique_ids,
+        }
+        for (i, layer_index) in enumerate(layer_indexes):
+            predictions["layer_output_%d" % i] = all_layers[layer_index]
+        output_spec = tf.contrib.tpu.TPUEstimatorSpec(
+            mode=mode, predictions=predictions, scaffold_fn=scaffold_fn)
+        return output_spec
+    return model_fn
 
 class CorefModel(object):
-
-    def model_fn_builder(bert_config, init_checkpoint, layer_indexes, use_tpu,
-                         use_one_hot_embeddings):
-        def model_fn(features, mode):  # pylint: disable=unused-argument
-            unique_ids = features["unique_ids"]
-            input_ids = features["input_ids"]
-            input_mask = features["input_mask"]
-            input_type_ids = features["input_type_ids"]
-            model = modeling.BertModel(
-                config=bert_config,
-                is_training=True,
-                input_ids=input_ids,
-                input_mask=input_mask,
-                token_type_ids=input_type_ids,
-                use_one_hot_embeddings=use_one_hot_embeddings)
-            if mode != tf.estimator.ModeKeys.PREDICT:
-                raise ValueError("Only PREDICT modes are supported: %s" % (mode))
-            tvars = tf.trainable_variables()
-            scaffold_fn = None
-            (assignment_map,
-             initialized_variable_names) = modeling.get_assignment_map_from_checkpoint(
-                 tvars, init_checkpoint)
-            if use_tpu:
-                def tpu_scaffold():
-                    tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
-                    return tf.train.Scaffold()
-                scaffold_fn = tpu_scaffold
-            else:
-                tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
-            tf.logging.info("**** Trainable Variables ****")
-            for var in tvars:
-                init_string = ""
-                if var.name in initialized_variable_names:
-                    init_string = ", *INIT_FROM_CKPT*"
-                tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape,
-                              init_string)
-            all_layers = model.get_all_encoder_layers()
-            predictions = {
-                "unique_id": unique_ids,
-            }
-            for (i, layer_index) in enumerate(layer_indexes):
-                predictions["layer_output_%d" % i] = all_layers[layer_index]
-            output_spec = tf.contrib.tpu.TPUEstimatorSpec(
-                mode=mode, predictions=predictions, scaffold_fn=scaffold_fn)
-            return output_spec
-        return model_fn
 
     def __init__(self, config):
         self.config = config
@@ -198,9 +199,16 @@ class CorefModel(object):
     def tensorize_example(self, example, is_training):
         clusters = example["clusters"]
         file_name = example["doc_key"]
-        import pdb; pdb.set_trace()
         if is_training:
           #embedding = self.train_file[file_name]
+            mapping = pickle.load(open("mapping.pickle", "rb"))
+            sentence = [" ".join(r) for r in example["sentences"]]
+            sentence = " ".join(sentence)
+            sentence = sentence.split(" ")
+            example["input_ids"] = [mapping[r] for r in sentence]
+            example["input_ids"] = list(set([mapping[r] for r in sentence]))
+            example["input_mask"] = tf.ones(shape=[1, len(sentence)], dtype=tf.int32)
+            example["input_type_ids"] = tf.zeros(shape=[1, len(sentence)], dtype=tf.int32)
             embedding = self.model_fn(example, tf.estimator.ModeKeys.PREDICT)
         else:
           #embedding = self.test_file[file_name]
